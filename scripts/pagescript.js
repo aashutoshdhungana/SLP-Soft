4a499aa7484b9c84a58f3b395b7e9c959ea36341
aashutoshdhungana <dhunganayt1205@gmail.com>
2021-09-05 20:40:22 +0545

    model pages done

--- scripts/pagescript.js
+++ scripts/pagescript.js
@@ -37,27 +37,39 @@ function getModelData() {
 
 // Open model pages
 function openModelPage(id) {
-    // Donot open if already open
+    if (document.getElementById(id+'-button')) {
+        changeModelPage(id);
+        return;
+    }
     let pageList = document.getElementById('pageOptionList');
-    let button = document.createElement('button');
+    let button = document.createElement('span');
     button.classList.add('page-tab');
     button.setAttribute('width', '32px');
     button.id = id + '-button';
-    button.innerHTML = modelPages.find(x => x.id === id).title;
-    button.onclick = () => {
+    let buttonText = document.createElement('span');
+    buttonText.classList.add('button-text');
+    buttonText.innerHTML = modelPages.find(x => x.id === id).title;
+    buttonText.onclick = () => {
         changeModelPage(id);
     }
+    button.appendChild(buttonText);
+    let cross = document.createElement('span');
+    cross.classList.add('button-cross');
+    cross.onclick = () => {
+        closeModelPage(id);
+    }
+    button.appendChild(cross);
     pageList.appendChild(button);
     changeModelPage(id); // this will save current and open next
 }
 
 //
-function saveModelPage(id) {
-    let index = modelPages.findIndex(x => x.id === id);
+function saveModelPage(modelId) {
+    let index = modelPages.findIndex(x => x.id === modelId);
     let diagramData = JSON.parse(document.getElementById('diagram').ej2_instances[0].saveDiagram());
     modelPages[index].diagram = diagramData;
 }
-function changeModelPage(id) {
+function changeModelPage(modelId) {
     let pageList = document.getElementById('pageOptionList');
     let activePage = pageList.querySelector('.active-model-page');
     if (activePage) {
@@ -66,10 +78,10 @@ function changeModelPage(id) {
         saveModelPage(activeButtonId);
     }
 
-    let newButton = pageList.querySelector(`#${id}-button`);
+    let newButton = pageList.querySelector(`#${modelId}-button`);
     newButton.classList.toggle('active-model-page');
     
-    let diagramData = modelPages.find(x => x.id === id).diagram;
+    let diagramData = modelPages.find(x => x.id === modelId).diagram;
     let diagramElement = document.getElementById('diagram').ej2_instances[0];
     diagramElement.isLoading = true;
     diagramElement.loadDiagram(JSON.stringify(diagramData))
@@ -77,14 +89,29 @@ function changeModelPage(id) {
     diagramElement.isLoading = false;
 }
 
-function closeModelPage() {
+function closeModelPage(modelId) {
+    let pageOptionList = document.getElementById('pageOptionList');
+
     // check if the model is last in the list
+    if (pageOptionList.childNodes.length <= 1) {
+        alert('Cannot Close!! \n At least one page needs to be open');
+    }
 
-    // if model is last in the list dont close otherwise
-    
-    // save currently active page
+    // if the model to be closed is active
+    let button = document.getElementById(modelId + '-button');
+    let nextPage = button.previousElementSibling;
+    if (!nextPage) {
+        nextPage = button.nextElementSibling;
+    }
 
-    // and append the next in the list
+    if (button.classList.contains('active-model-page')) {
+        changeModelPage(nextPage.id.replace('-button', ''));
+    }
+
+    else {
+        saveModelPage(modelId);
+    }
+    button.remove();
 }
 // when clicked on the model from the list the corresponding diagram opens and adds itself to the page list in the top
 // when switching pages similar searches from the list and appends the json data of the diagram
